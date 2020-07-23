6d12c99f8ed6ef0582c19c1c85fec948bb440b94
Gabriel Porras <ghporras@gmail.com>
2020-07-22 20:35:42 -0500

    feat: Work on project. Stage 5/5 - End

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,27 +1,63 @@
-print('Hello! My name is Gabotron.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-name = input()
 
-print('What a great name you have, ' + name + '!')
-print('Let me guess your age.')
-print('Enter remainders of dividing your age by 3, 5 and 7.')
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-rem3 = int(input())
-rem5 = int(input())
-rem7 = int(input())
 
-age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-print("Your age is " + str(age) + "; that's a good time to start programming!")
-print('Now I will prove to you that I can count to any number you want.')
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-# read a number and count to it here
-count_number = int(input())
-i = 0
-while i <= count_number:
-    print(f"{i} !")
-    i += 1
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
 
-print('Completed, have a nice day!')
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    # write your code here
+    print('Completed, have a nice day!')
+
+
+def user_test():
+    print("Let's test your programming knowledge.")
+    print("Why do we use methods?")
+    print("1. To repeat a statement multiple times.")
+    print("2. To decompose a program into several small subroutines.")
+    print("3. To determine the execution time of a program.")
+    print("4. To interrupt the execution of a program.")
+    user_answer = input()
+    while user_answer != "2":
+        print("Please, try again.")
+        user_answer = input()
+    print("Completed, have a nice day!")
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Gabotron', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+user_test()
+end()
